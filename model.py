bad0ed0a6b7eda516ab9c1ac16c7d4845a7f7397
zhuko <zhukov_ai@outlook.com>
2024-04-11 17:46:06 +0300

    тра та ти тра та туй

--- model.py
+++ model.py
@@ -5,15 +5,17 @@ rect_umbrella = pygame.Rect(585, 775, 125, 125)
 rect_bucket = pygame.Rect(485, 855, 60, 60)
 visibility = False
 
+
 def kot_xodit_vpravo():
     rect_cot.x += 20
     rect_umbrella.x += 20
     rect_bucket.x += 20
+
 def kot_xodit_vlevo():
     rect_cot.x -= 20
     rect_umbrella.x -= 20
     rect_bucket.x -= 20
     if rect_bucket.x <= 0:
-        rect_cot.x = 0
-        rect_umbrella.x = 0
+        rect_cot.x = 15
+        rect_umbrella.x = 100
         rect_bucket.x = 0
