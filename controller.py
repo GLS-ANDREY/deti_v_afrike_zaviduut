633495ef6f11b8576349d27eb9ccac2ea5f75359
zhuko <zhukov_ai@outlook.com>
2024-04-23 17:18:47 +0300

    тра та ти тра та туй

--- controller.py
+++ controller.py
@@ -6,14 +6,11 @@ def allsobitiya():
     s = pygame.event.get()
 
     for a in s:
-        if a.type == pygame.KEYUP and a.key == pygame.K_SPACE:
-            model.perevorot = not model.perevorot
-            model.perevernutie_predmeti()
+        if a.type == pygame.KEYDOWN and a.key == pygame.K_RIGHT:
+            model.kot_pravo()
+        if a.type == pygame.KEYDOWN and a.key == pygame.K_LEFT:
+            model.kot_levo()
         if a.type == pygame.KEYUP and a.key == pygame.K_q:
             model.visibility = not model.visibility
-        if a.type == pygame.KEYDOWN and a.key == pygame.K_RIGHT:
-            model.kot_xodit_vpravo()
-        if a.type == pygame.KEYDOWN and a.key == pygame.K_LEFT:
-            model.kot_xodit_vlevo()
         if a.type == pygame.QUIT:
             exit()
--- model.py
+++ model.py
@@ -7,26 +7,31 @@ visibility = False
 perevorot = False#True - vpravo, False - vlevo
 
 
-def kot_xodit_vpravo():
+def kot_pravo():
+    global perevorot
+    perevorot = True
+    #перемещение предметов
     rect_cot.x += 20
     rect_umbrella.x += 20
     rect_bucket.x += 20
-
-def kot_xodit_vlevo():
+    #переворачивание предметов
+    if rect_umbrella.x >= rect_cot.x:
+        rect_umbrella.x -= 120
+    if rect_bucket.x <= rect_cot.x:
+        rect_bucket.x += 140
+def kot_levo():
+    global perevorot
+    perevorot = False
+    # перемещение предметов
     rect_cot.x -= 20
     rect_umbrella.x -= 20
     rect_bucket.x -= 20
+    # переворачивание предметов
     if rect_bucket.x <= 0:
         rect_cot.x = 15
         rect_umbrella.x = 100
         rect_bucket.x = 0
-
-def perevernutie_predmeti():
     if rect_umbrella.x <= rect_cot.x:
         rect_umbrella.x += 120
-    else:
-        rect_umbrella.x -= 120
     if rect_bucket.x >= rect_cot.x:
         rect_bucket.x -= 140
-    else:
-        rect_bucket.x += 140
