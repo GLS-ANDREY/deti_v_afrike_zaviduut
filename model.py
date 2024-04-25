9f631b4a076c6887c9d97721e4f53ab033dc0ff0
zhuko <zhukov_ai@outlook.com>
2024-04-25 17:46:50 +0300

    тра та ти тра та туй

--- model.py
+++ model.py
@@ -4,21 +4,28 @@ rect_cot = pygame.Rect(500, 874, 167, 126)
 rect_umbrella = pygame.Rect(585, 775, 125, 125)
 rect_bucket = pygame.Rect(485, 855, 60, 60)
 visibility = False
-perevorot = False#True - vpravo, False - vlevo
+perevorot = False  # True - vpravo, False - vlevo
 
 
 def kot_pravo():
     global perevorot
     perevorot = True
-    #перемещение предметов
+    # перемещение предметов
     rect_cot.x += 20
     rect_umbrella.x += 20
     rect_bucket.x += 20
-    #переворачивание предметов
-    if rect_umbrella.x >= rect_cot.x:
-        rect_umbrella.x -= 120
-    if rect_bucket.x <= rect_cot.x:
-        rect_bucket.x += 140
+    # граница
+    if rect_bucket.x >= 940:
+        rect_cot.x = 818
+        rect_umbrella.x = 790
+        rect_bucket.x = 940
+    # переворачивание предметов
+    if rect_umbrella.x >= rect_bucket.x:
+        rect_umbrella.x -= 250
+    if rect_cot.x >= rect_bucket.x:
+        rect_cot.x -= 140
+
+
 def kot_levo():
     global perevorot
     perevorot = False
@@ -26,12 +33,13 @@ def kot_levo():
     rect_cot.x -= 20
     rect_umbrella.x -= 20
     rect_bucket.x -= 20
-    # переворачивание предметов
+    # граница
     if rect_bucket.x <= 0:
         rect_cot.x = 15
         rect_umbrella.x = 100
         rect_bucket.x = 0
-    if rect_umbrella.x <= rect_cot.x:
-        rect_umbrella.x += 120
-    if rect_bucket.x >= rect_cot.x:
-        rect_bucket.x -= 140
+    # переворачивание предметов
+    if rect_umbrella.x <= rect_bucket.x:
+        rect_umbrella.x += 250
+    if rect_cot.x <= rect_bucket.x:
+        rect_cot.x += 140
--- view.py
+++ view.py
@@ -4,21 +4,30 @@ display = pygame.display.set_mode([1000, 1000])
 cat1 = pygame.image.load("pics/cat1.png")
 umbrella = pygame.image.load("pics/umbrella.png")
 bucket = pygame.image.load("pics/bucket.png")
+tuchka = pygame.image.load("pics/cloud.png")
 transform_umbrella = pygame.transform.scale(umbrella, [model.rect_umbrella.width, model.rect_umbrella.height])
 transform_bucket = pygame.transform.scale(bucket, [model.rect_bucket.width, model.rect_bucket.height])
-
+#1if. если висибле равно труе то рисуем прямогульники
+#2if. если переворот фолс то рисуем кота смотрящего влево
+#3if. если переворот труе то рисуем кота смотрящего вправо
 
 def risovanie():
     display.fill([0, 0, 0])
-    if model.visibility == True:
+    if model.visibility == True and model.perevorot == False:
         pygame.draw.rect(display, [255, 0, 9], model.rect_cot, 3)
         pygame.draw.rect(display, [64, 150, 193], model.rect_umbrella, 3)
         pygame.draw.rect(display, [73, 156, 73], model.rect_bucket, 3)
+        display.blit(cat1, model.rect_cot)
+        display.blit(transform_umbrella, model.rect_umbrella)
+        display.blit(transform_bucket, model.rect_bucket)
     if model.visibility == False and model.perevorot == False:
         display.blit(cat1, model.rect_cot)
         display.blit(transform_umbrella, model.rect_umbrella)
         display.blit(transform_bucket, model.rect_bucket)
     if model.perevorot == True:
+        pygame.draw.rect(display, [255, 0, 9], model.rect_cot, 3)
+        pygame.draw.rect(display, [64, 150, 193], model.rect_umbrella, 3)
+        pygame.draw.rect(display, [73, 156, 73], model.rect_bucket, 3)
         perevernutiy_kot = pygame.transform.flip(cat1,True,False)
         perevernutoe_bucket = pygame.transform.flip(transform_bucket,True,False)
         perevernutoe_umbrella = pygame.transform.flip(transform_umbrella, True, False)
